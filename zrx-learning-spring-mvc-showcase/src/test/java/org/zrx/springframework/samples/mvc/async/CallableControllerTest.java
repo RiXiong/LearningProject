c21a9dcf40f525b4d2e9b962f2dd016ea3d95ae4
rixiong <827935210@qq.com>
2016-09-08 20:53:42 +0800

    springMVC 测试用例 Signed-off-by: rixiong <827935210@qq.com>

--- zrx-learning-spring-mvc-showcase/src/test/java/org/zrx/springframework/samples/mvc/async/CallableControllerTest.java
+++ zrx-learning-spring-mvc-showcase/src/test/java/org/zrx/springframework/samples/mvc/async/CallableControllerTest.java
@@ -1,44 +1,77 @@
 package org.zrx.springframework.samples.mvc.async;
 
+import static org.hamcrest.Matchers.instanceOf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.zrx.springframework.samples.mvc.AbstractContextControllerTests;
 
-import static org.junit.Assert.*;
 
 /**
  * Fynction:    R on 2016/9/5.
  * Author:      zhangrixiong
  * DateTime:    2016/9/5 20:59
  */
-public class CallableControllerTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CallableControllerTest extends AbstractContextControllerTests {
+
+    private MockMvc mockMvc;
+
     @Before
-    public void setUp() throws Exception {
-
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(this.wac).build();
     }
 
     @Test
-    public void callable() throws Exception {
+    public void responseBody() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/async/callable/response-body"))
+                .andExpect(request().asyncStarted())
+                .andExpect(request().asyncResult("Callable result"))
+                .andReturn();
 
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(content().string("Callable result"));
     }
 
     @Test
-    public void callablewithView() throws Exception {
+    public void view() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/async/callable/view"))
+                .andExpect(request().asyncStarted())
+                .andExpect(request().asyncResult("views/html"))
+                .andReturn();
 
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/views/views/html.jsp"))
+                .andExpect(model().attribute("foo", "bar"))
+                .andExpect(model().attribute("fruit", "apple"));
     }
 
     @Test
-    public void callableWithException() throws Exception {
+    public void exception() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/async/callable/exception"))
+                .andExpect(request().asyncStarted())
+                .andExpect(request().asyncResult(instanceOf(IllegalStateException.class)))
+                .andReturn();
 
-    }
-
-    @Test
-    public void callableWithCustomTimeoutHandling() throws Exception {
-
-    }
-
-    @Test
-    public void handleException() throws Exception {
-
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(content().string("Handled exception: Callable error"));
     }
 
 }
